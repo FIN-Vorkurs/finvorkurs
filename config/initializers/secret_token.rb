288bc58b9ecfc04dfd0e11970fca6b6065c92aff
Martin Knoll <martinknoll@me.com>
2012-02-24 19:55:50 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VorkursHomepage::Application.config.secret_token = 'fbdb892a7ead27a8ae850d2e8baac3678a6889e4b39f17d047608b7d5fab4055f1932f2c35611b508a22eb1ef8c60dfdb5efe2cda32ca3cdf4e9fca4a994ed61'
