2ddc3c2866a5f41075e0c149e9a68a2bb01f10e3
Martin Knoll <martinknoll@me.com>
2012-04-03 17:56:36 +0200

    user bar logout & user edit link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,18 @@
 class UsersController < ApplicationController
-	before_filter :authenticate_user!, :except => [:new, :create]
-	def new
-		@user = User.new
-	end
+  before_filter :authenticate_user!, :except => [:new, :create]
+  def new
+    @user = User.new
+  end
 
-	def create
-		@user = User.new(params[:user])
-		if @user.save
-			redirect_to root_url, :notice => "Registrierung erfolgreich!"
-		else
-			render "new"
-		end
-	end
+  def edit
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to root_url, :notice => "Registrierung erfolgreich!"
+    else
+      render "new"
+    end
+  end
 end
