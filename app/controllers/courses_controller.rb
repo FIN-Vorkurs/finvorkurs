5ae56603041496251a59af84bbf778103e429472
Martin Knoll <martinknoll@me.com>
2012-07-15 18:42:29 +0200

    show courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,11 @@
 class CoursesController < ApplicationController
+  before_filter :authenticate_admin!, only: [:show]
   def index
     @user = current_user
     @courses = Course.all
+  end
+
+  def show
+    @course = Course.find params[:id]
   end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,5 @@
 class Course < ActiveRecord::Base
   has_many :enrollments
+  has_many :users, through: :enrollments
   belongs_to :test
 end
