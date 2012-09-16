bdc9a9d96a6d9655aafb8ec40c456cae6dc435f8
Martin Knoll <martinknoll@me.com>
2012-09-16 19:13:20 +0200

    group syntax fnord

--- app/admin/tickets.rb
+++ app/admin/tickets.rb
@@ -1,7 +1,7 @@
  ActiveAdmin.register_page "Tickets" do
   content do
     table do
-      User.all(joins: :enrollments, group: :user_id).each do |user|
+      User.all(joins: :enrollments, group: 'users.id').each do |user|
         render partial: 'ticket', locals: {user: user}
       end
     end
