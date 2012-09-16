8f0a1f3baf2fe6dd8c35b496f2490af720f69ad2
Martin Knoll <martinknoll@me.com>
2012-09-16 19:27:28 +0200

    typo

--- app/admin/tickets.rb
+++ app/admin/tickets.rb
@@ -1,7 +1,7 @@
  ActiveAdmin.register_page "Tickets" do
   content do
     table do
-      User.all(joins: :enrollments, group: 'users.id, user.name').each do |user|
+      User.all(joins: :enrollments, group: 'users.id, users.name').each do |user|
         render partial: 'ticket', locals: {user: user}
       end
     end
