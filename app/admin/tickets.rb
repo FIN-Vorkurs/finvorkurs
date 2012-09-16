2cd7e19af3d5fa6cb655c72a4018eb0b0bcf98a7
Martin Knoll <martinknoll@me.com>
2012-09-17 00:26:47 +0200

    users.id ASC

--- app/admin/tickets.rb
+++ app/admin/tickets.rb
@@ -1,6 +1,6 @@
  ActiveAdmin.register_page "Tickets" do
   content do
-    User.all(select: 'DISTINCT users.*', joins: :enrollments).each do |user|
+    User.all(select: 'DISTINCT users.*', joins: :enrollments, order: 'users.id ASC').each do |user|
       render partial: 'ticket', locals: {user: user}
     end
   end
