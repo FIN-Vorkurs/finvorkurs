1913b785ac10c4432e31589a337f11d66c0d3296
Martin Knoll <martinknoll@me.com>
2012-09-16 02:28:10 +0200

    score

--- app/admin/courses.rb
+++ app/admin/courses.rb
@@ -20,8 +20,11 @@ ActiveAdmin.register Course do
     end
 
     panel pluralize course.enrollments.count, "Enrollment" do
-      table_for Enrollment.all(joins: {user: :test_results}, conditions: {course_id: course.id, 'test_results.course_id' => course.id}, order: 'test_results.score ASC') do |enrollment|
+      table_for Enrollment.all(joins: {user: :test_results}, conditions: {course_id: course.id, 'test_results.course_id' => course.id}, order: 'test_results.score ASC') do
         column :user
+        column 'Score' do |enrollment|
+          enrollment.user.test_results.where('course_id = ?', enrollment.course.id).first.score
+        end
         column :created_at
       end
     end
