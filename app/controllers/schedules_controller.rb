ea49038f50a8c5e30436b70494d5ec3ca0645807
Martin Knoll <martinknoll@me.com>
2012-09-16 21:57:43 +0200

    sort_by

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -2,6 +2,6 @@ class SchedulesController < ApplicationController
   def index
     @schedule = []
     @schedule += Soiree.all
-    @schedule.sort_by(&:date)
+    @schedule.sort_by!(&:date)
   end
 end
