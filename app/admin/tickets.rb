1160516832b098e355e57e13ef15c88eb92af8d6
Martin Knoll <martinknoll@me.com>
2012-09-16 19:49:03 +0200

    multiple tables

--- app/admin/tickets.rb
+++ app/admin/tickets.rb
@@ -1,9 +1,7 @@
  ActiveAdmin.register_page "Tickets" do
   content do
-    table do
-      User.all(select: 'DISTINCT users.*', joins: :enrollments).each do |user|
-        render partial: 'ticket', locals: {user: user}
-      end
+    User.all(select: 'DISTINCT users.*', joins: :enrollments).each do |user|
+      render partial: 'ticket', locals: {user: user}
     end
   end
 end
