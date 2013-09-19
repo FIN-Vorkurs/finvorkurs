77686841a0e6e07a9ec39f6b9d5ea32518afcb4b
Martin Knoll <martinknoll@me.com>
2013-09-19 18:29:11 +0200

    really fixed:   app/models/enrollment.rb

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -12,7 +12,7 @@ class Enrollment < ActiveRecord::Base
     if self.user.test_results.where('course_id = ?', self.course.id).empty?
       "#{self.user.name} #{self.group}"
     else
-      "#{self.user.name} (#{self.user.test_results.where('course_id = ?', self.course.id).first.score}% #{self.group} #{self.user.answers.where('question_id = ?', 31).first.text} #{self.user.answers.where('question_id = ?', 32).first.text})"
+      "#{self.user.name} (#{self.user.test_results.where('course_id = ?', self.course.id).first.score}% #{self.group} #{if self.user.answers.where('question_id = ?', 31).first then self.user.answers.where('question_id = ?', 31).first.text end} #{if self.user.answers.where('question_id = ?', 32).first then self.user.answers.where('question_id = ?', 32).first.text end})"
     end
   end
 
