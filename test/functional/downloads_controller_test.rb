7e2ff617590159960a4806b3f0c070e09830ca74
Martin Knoll <martinknoll@me.com>
2013-10-01 17:23:30 +0200

    fixed test
    because downloads are no accessible for non registered users, redirect
    is the expected behavior
    modified:   test/functional/downloads_controller_test.rb

--- test/functional/downloads_controller_test.rb
+++ test/functional/downloads_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class DownloadsControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
-    assert_response :success
+    assert_response :redirect
   end
 
 end
